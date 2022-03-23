c0b5c6613458146bfd445e74cbe12344ab502a23
Hugh McCreery <hughmccreery@Hughs-MacBook-Pro.local>
2022-03-22 23:35:07 -0400

    also added range levels to team comp totals

--- R/generateTeamComparisonWeaponTable.R
+++ R/generateTeamComparisonWeaponTable.R
@@ -132,7 +132,7 @@ generateTeamComparisonWeaponTable <- function(
       )
     ) %>%
     summary_rows(
-      columns = vars(kpg, apg, dpg, iapg),
+      columns = vars(kpg, apg, dpg, iapg, weapon_Range),
       fns = list(Tot = ~mean(.)),
       formatter = fmt_number,
       decimals = 1
